389bb4ca14c1255fbf7a641c774c21418a1ec9a1
Lisyak Yevhenii <lisyak.yevhenii@gmail.com>
2016-11-09 01:22:04 +0200

    Stuff. lesson 1

---
+++ DevJava/src/ua/java/lesson1/Main.java
@@ -0,0 +1,12 @@
+package ua.java.lesson1;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 13;
+		double c = a + b;
+		System.out.println(c);
+		System.out.println(b);
+	}
+}
+
